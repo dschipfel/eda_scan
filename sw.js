8eca8f44949dcac3e69d4f88c977f6d12fcad445
Daniel Schipfel <daniel@schipfel.de>
2026-09-03 21:52:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 // Version und durchläuft den Install/Activate-Zyklus erneut -- sonst bleibt für immer die
 // zuerst installierte Version aktiv, egal wie oft index.html aktualisiert wird.
 
-const CACHE_NAME = 'pp-erfassung-v4';
+const CACHE_NAME = 'pp-erfassung-v5';
 const PRECACHE_URLS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
